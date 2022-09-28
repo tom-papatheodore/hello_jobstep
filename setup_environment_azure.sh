6b3cd7d4e99af1c13c26c82cb11497ed4ea76ec7
Tom Papatheodore <papatheodore@ornl.gov>
2022-09-28 16:41:11 +0000

    added Azure-specific version

---
+++ setup_environment_azure.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+module load mpi/openmpi-4.1.3
+
+export PATH=/usr/local/cuda-11.6/bin:${PATH}
--- submit_azure.sh
+++ submit_azure.sh
@@ -2,14 +2,12 @@
 
 #SBATCH -N 1
 #SBATCH -t 10
-#SBATCH -p eval-gpu
+#SBATCH -p hpc
 #SBATCH --exclusive
 
-source ./setup_environment_aws.sh
+source ./setup_environment.sh
 
-export OMP_NUM_THREADS=6
+export OMP_NUM_THREADS=1
 export OMP_PROC_BIND=close
-
-export I_MPI_PMI_LIBRARY=/opt/slurm/lib/libpmi.so
 
 srun -n8 --ntasks-per-node=8 -c6 --gpus-per-node=8 --gpu-bind=map_gpu:0,1,2,3,4,5,6,7 --cpu-bind=mask_cpu:0x3f,0xfc0,0x3f000,0xfc0000,0x3f000000,0xfc0000000,0x3f000000000,0xfc0000000000 ./hello_jobstep | sort
